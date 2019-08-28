bdc3527e9cf7dec69add98425c98204ce84602e5
mawwy <martin.a.wieland@gmx.de>
2019-08-28 20:07:16 +0200

    moved while loop to more explanations

--- ocr_to_messenger.py
+++ ocr_to_messenger.py
@@ -9,6 +9,12 @@ from telegram.ext import CommandHandler
 import logging
 
 from private_stuff import BOT_TOKEN
+# telegram_token = BOT_TOKEN
+
+# ENTER YOUR TOKEN HERE
+# ENTER YOUR TOKEN HERE
+# ENTER YOUR TOKEN HERE
+telegram_token = "TOKEN"
 
 ALERT_SPAM_INTERVAL = 15  # time between messages when queue is over to WAKE YOU UP
 WAKE_UP_MESSAGE = "YOU WANT TO WAIT ANOTHER 10 HOURS? NO? WAKE THE HELL UP"
@@ -52,8 +58,8 @@ while test_run:
 
     cv2.imshow('preview', test_hsv_image)
     # character recognition happens HERE
-    ocr_string = pytesseract.image_to_string(test_hsv_image)
-    print(ocr_string)
+    test_ocr_string = pytesseract.image_to_string(test_hsv_image)
+    print(test_ocr_string)
 
     if cv2.waitKey(25) and 0xFF == ord('q'):
         cv2.destroyAllWindows()
@@ -62,8 +68,31 @@ while test_run:
     sleep(1)
 
 
-def spam_user(bot, job):
-    bot.send_message(job.context, text=WAKE_UP_MESSAGE)
+def spam_user_if_no_queue(bot, job):
+
+    def check_if_queue_inactive():
+        queue_is_active = True
+
+        np_image_grab = np.array(ImageGrab.grab(bbox=queue_msg_box))
+
+        hsv_image = cv2.cvtColor(np_image_grab, cv2.COLOR_BGR2HSV)
+        # cv2.imshow('preview', hsv_image)
+
+        # character recognition happens HERE
+        ocr_string = pytesseract.image_to_string(hsv_image)
+        ocr_length = len(ocr_string)
+        print(ocr_length)
+
+        if ocr_length < 35:
+            queue_is_active = False
+
+        if not queue_is_active:
+            return True
+        else:
+            return False
+
+    if check_if_queue_inactive():
+        bot.send_message(job.context, text=WAKE_UP_MESSAGE)
 
 
 def get_queue_postion_from_screen():
@@ -84,47 +113,15 @@ def get_position(bot, update):
 
 def start_alarm(bot, update, job_queue, chat_data):
 
-    def check_if_queue_inactive():
-        queue_is_active = True
-
-        np_image_grab = np.array(ImageGrab.grab(bbox=queue_msg_box))
-
-        hsv_image = cv2.cvtColor(np_image_grab, cv2.COLOR_BGR2HSV)
-        # cv2.imshow('preview', hsv_image)
-
-        # character recognition happens HERE
-        ocr_string = pytesseract.image_to_string(hsv_image)
-        ocr_length = len(ocr_string)
-        print(ocr_length)
-
-        if ocr_length < 40:
-            queue_is_active = False
-
-        if not queue_is_active:
-            return True
-        else:
-            return False
-
     chat_id = update.message.chat_id
-
     update.message.reply_text('Alarm successfully started!')
-
-    queue_active = True
-
-    while queue_active:
-        if check_if_queue_inactive():
-            queue_active = False
-        else:
-            sleep(5)
-    else:
-        # executed when while condition becomes false
-        job = job_queue.run_repeating(spam_user, ALERT_SPAM_INTERVAL, context=chat_id)
-        chat_data['job'] = job
+    job = job_queue.run_repeating(spam_user_if_no_queue, ALERT_SPAM_INTERVAL, context=chat_id)
+    chat_data['job'] = job
 
 
 def stop_alarm(bot, update, chat_data):
     if 'job' not in chat_data:
-        update.message.reply_text('You have no active timer')
+        update.message.reply_text('You have no active alarm')
         return
 
     job = chat_data['job']
@@ -145,8 +142,7 @@ def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 
-# enter your token here
-telegram_token = BOT_TOKEN
+
 
 updater = Updater(token=telegram_token)
 dispatcher = updater.dispatcher
@@ -157,7 +153,7 @@ start_alarm_handler = CommandHandler('startalarm',
                                      start_alarm,
                                      pass_job_queue=True,
                                      pass_chat_data=True)
-stop_alarm_handler = CommandHandler("unset", stop_alarm, pass_chat_data=True)
+stop_alarm_handler = CommandHandler("stopalarm", stop_alarm, pass_chat_data=True)
 
 
 dispatcher.add_handler(start_handler)
